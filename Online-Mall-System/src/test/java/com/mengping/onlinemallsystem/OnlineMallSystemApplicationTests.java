5e418db55a1c58eb6dcc8db2555919a47e1e8657
MengPing_孟頻 <mengping0110@gmail.com>
2023-06-01 18:29:32 +0800

    初始化 Spring Boot project

---
+++ Online-Mall-System/src/test/java/com/mengping/onlinemallsystem/OnlineMallSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengping.onlinemallsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMallSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
